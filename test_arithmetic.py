f1f006fdf1827fc7deb029240bd8a44be4502ed5
Iris Kim <ikim1@godaddy.com>
2022-06-03 10:54:26 -0400

    Fail unit tests

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -5,7 +5,7 @@ def subtract(a, b):
     return a - b
 
 def test_add():
-    assert add(1,2) == 3
+    assert add(1,2) == 1
 
 def test_subtract():
-    assert subtract(1,2) == -1
+    assert subtract(1,2) == 0
